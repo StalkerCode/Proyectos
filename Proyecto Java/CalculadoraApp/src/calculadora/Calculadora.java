670097a2e4557ed47e404b2a5fc995a1a3dd2ad9
CodeTornadoStorms <demyu369@gmail.com>
2024-10-24 13:11:16 -0500

    Java Calculadora clase Calculadora menu

--- Proyecto Java/CalculadoraApp/src/calculadora/Calculadora.java
+++ Proyecto Java/CalculadoraApp/src/calculadora/Calculadora.java
@@ -4,5 +4,16 @@ import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Calculadora {
+	private static Scanner Entrada = new Scanner(System.in);
+	private static PrintStream Salida = new PrintStream(System.out);
+
+	public void menu() {
+		Salida.println("**** Aplicacion de Calculadora ****");
+		Salida.println("1. Suma");
+		Salida.println("2. Resta");
+		Salida.println("3. Multiplicacion");
+		Salida.println("4. Division");
+		Salida.println("5. Salir");
+	}
 
 }
