69a792c9862db4075327da752ec3fd30b0db4565
CodeTornadoStorms <demyu369@gmail.com>
2024-10-24 13:31:47 -0500

    Java ListaPersona inicio

---
+++ Proyecto Java/ListaPersonaApp/src/persona/Persona.java
@@ -0,0 +1,12 @@
+package persona;
+
+
+public class Persona {
+	private int id;
+	private String nombre;
+	private String telefono;
+	private String email;
+	static private int numeroPersona = 0;
+
+
+}
