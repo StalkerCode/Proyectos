4f7a4616298477573aed31e67c4fa2bcae0d5672
CodeTornadoStorms <demyu369@gmail.com>
2024-10-24 13:12:26 -0500

    Java Calculadora clase Calculadora funcionalidades

--- Proyecto Java/CalculadoraApp/src/calculadora/Calculadora.java
+++ Proyecto Java/CalculadoraApp/src/calculadora/Calculadora.java
@@ -50,4 +50,58 @@ public class Calculadora {
 		return numero;
 	}
 
+	public Double sumar(Double num1, Double num2) {
+		return num1 + num2;
+	}
+
+	public Double restar(Double num1, Double num2) {
+		return num1 - num2;
+	}
+
+	public Double multiplicar(Double num1, Double num2) {
+		return num1 * num2;
+	}
+
+	public Double dividir(Double num1, Double num2) {
+		if (num2 == 0) {
+			Salida.println("Error.\nNo se puede dividir entre 0.");
+			return null;
+		}
+		return num1 / num2;
+	}
+
+	public void salir() {
+		Salida.println("Gracias por usar la calculadora. Adiós!");
+	}
+
+	public void operarSuma() {
+		Double num1 = NumeroReal();
+		Double num2 = NumeroReal();
+		Double resultado = sumar(num1, num2);
+		Salida.println("\nEl resultado de la suma es: " + resultado);
+	}
+
+	public void operarResta() {
+		Double num1 = NumeroReal();
+		Double num2 = NumeroReal();
+		Double resultado = restar(num1, num2);
+		Salida.println("\nEl resultado de la resta es: " + resultado);
+	}
+
+	public void operarMultiplicacion() {
+		Double num1 = NumeroReal();
+		Double num2 = NumeroReal();
+		Double resultado = multiplicar(num1, num2);
+		Salida.println("\nEl resultado de la multiplicación es: " + resultado);
+	}
+
+	public void operarDivision() {
+		Double num1 = NumeroReal();
+		Double num2 = NumeroReal();
+		Double resultado = dividir(num1, num2);
+		if (resultado != null) {
+			Salida.println("\nEl resultado de la division es: " + resultado);
+		}
+	}
+
 }
