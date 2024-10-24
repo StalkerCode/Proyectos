89ef0832fd79efd4c97b2d03de0d56145731a44c
CodeTornadoStorms <demyu369@gmail.com>
2024-10-24 13:14:52 -0500

    Java Calculadora Main funcionalidades

---
+++ Proyecto Java/CalculadoraApp/src/mainApp/CalculadoraMain.java
@@ -0,0 +1,54 @@
+package mainApp;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import calculadora.Calculadora;
+
+public class CalculadoraMain {
+	static Scanner Entrada = new Scanner(System.in);
+	static PrintStream Salida = new PrintStream(System.out);
+
+	public static void main(String[] args) {
+		int op;
+		Calculadora cal= new Calculadora();
+		do {
+			cal.menu();
+			switch (op=cal.pedirNumeroEntero()) {
+			case 1:
+				cal.operarSuma();
+				Salida.println("");
+				break;
+
+			case 2:
+				cal.operarResta();
+				Salida.println("");
+				break;
+
+			case 3:
+				cal.operarMultiplicacion();
+				Salida.println("");
+				break;
+
+			case 4:
+				cal.operarDivision();
+				Salida.println("");
+				break;
+
+			case 5:
+				cal.salir();
+				break;
+
+			default:
+				Salida.println("Opción incorrecta. Intente nuevamente.");
+				Salida.println("");
+				break;
+			}
+		}while(op!=5);
+
+	}
+
+
+	}
+
+}
