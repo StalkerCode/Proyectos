bb0f5f520ed66f3019e13aa0946f435fe2ee2931
StalkerCode <demyu369@gmail.com>
2024-11-04 18:02:43 -0500

    Inicio C Calculadora Basica Funciones, menu

--- Proyecto C/Calculadora Basica/CalculadoraBasica.c
+++ Proyecto C/Calculadora Basica/CalculadoraBasica.c
@@ -1,4 +1,22 @@
 #include "stdio.h"
+#include "ctype.h"
+
+float sum(float a, float b);
+float rest(float a, float b);
+float mult(float a, float b);
+float div(float a, float b);
+
+int ingresar_entero();
+
+void limpiarBuffer();
+void menu();
+
+int main()
+{
+    ingresar_entero();
+
+    return 0;
+}
 
 float sum(float a, float b)
 {
@@ -25,8 +43,31 @@ float div(float a, float b)
     return a / b;
 }
 
-int main()
-{
+void menu(){
+    printf("1. Suma\n");
+    printf("2. Resta\n");
+    printf("3. Multiplicacion\n");
+    printf("4. Division\n");
+    printf("5. Exit\n");
+    printf("Elije: ");
+}
 
-    return 0;
+int ingresar_entero(){
+    char num;
+    printf("ingrese una opcion(numero entero): ");
+    scanf("%c", &num);
+    limpiarBuffer();
+    while (!isdigit(num))
+    {
+        printf("Error: Ingreso incorrecto, intente de nuevo.\n");
+        printf("ingrese una opcion(numero entero): ");
+        scanf("%c", &num);
+        limpiarBuffer();
+    }
+    return num;
+}
+
+void limpiarBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
