7eaa289e0b821c86aa127316f9cc16dbf71dab69
StalkerCode <demyu369@gmail.com>
2024-11-11 11:41:17 -0500

    C fin Conversor de Temperatura

--- Proyecto C/Conversor de Temperatura/ConversorTemperatura.c
+++ Proyecto C/Conversor de Temperatura/ConversorTemperatura.c
@@ -9,11 +9,27 @@ float fahrenheit_to_kelvin(float fahrenheit);
 float kelvin_to_fahrenheit(float kelvin);
 
 void menu();
-void limpiarBuffer();
 void limpiar_pantalla();
+void opciones(int opcion);
+float validarEntradaFloat();
+int validarEntradaInt();
+void pausa();
+
 
 int main()
 {
+    int op;
+    printf("Bienvenido a la Conversor de Temperatura\n");
+    do
+    {
+        menu();
+        op = validarEntradaInt();
+        opciones(op);
+        limpiar_pantalla();
+    } while (op != 0);
+
+    printf("\n\nGracias por usar la Conversor de Temperatura\n\n\n");
+
     return 0;
 }
 
@@ -52,69 +68,106 @@ void menu()
     printf("6. Kelvin to Fahrenheit\n");
     printf("0. Salir\n");
 }
-void limpiarBuffer()
-{
-    char c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
+
 void limpiar_pantalla()
 {
     printf("\033[2J"); // Limpia la pantalla
     printf("\033[H");  // Mueve el cursor al inicio
 }
 
-void opciones(int opcion){
+void opciones(int opcion)
+{
     float celsius, fahrenheit, kelvin;
     switch (opcion)
     {
+    case 0:
+        printf("\n\nHasta luego\n\n");
+        pausa();
+        break;
     case 1:
         printf("Ingrese la temperatura en Celsius: ");
-        celsius=validarEntradaFloat();
-        printf("La temperatura en Fahrenheit es: %.2f\n", celsius_to_fahrenheit(celsius));
+        celsius = validarEntradaFloat();
+        printf("\nLa temperatura en Fahrenheit es: %.2f\n", celsius_to_fahrenheit(celsius));
+        pausa();
         break;
     case 2:
         printf("Ingrese la temperatura en Fahrenheit: ");
-        fahrenheit=validarEntradaFloat();
-        printf("La temperatura en Celsius es: %.2f\n", fahrenheit_to_celsius(fahrenheit));
+        fahrenheit = validarEntradaFloat();
+        printf("\nLa temperatura en Celsius es: %.2f\n", fahrenheit_to_celsius(fahrenheit));
+        pausa();
         break;
     case 3:
         printf("Ingrese la temperatura en Celsius: ");
-        celsius=validarEntradaFloat();
-        printf("La temperatura en Kelvin es: %.2f\n", celsius_to_kelvin(celsius));
+        celsius = validarEntradaFloat();
+        printf("\nLa temperatura en Kelvin es: %.2f\n", celsius_to_kelvin(celsius));
+        pausa();
         break;
     case 4:
         printf("Ingrese la temperatura en Kelvin: ");
-        kelvin=validarEntradaFloat();
-        printf("La temperatura en Celsius es: %.2f\n", kelvin_to_celsius(kelvin));
+        kelvin = validarEntradaFloat();
+        printf("\nLa temperatura en Celsius es: %.2f\n", kelvin_to_celsius(kelvin));
+        pausa();
         break;
     case 5:
-        printf("Ingrese la temperatura en Fahrenheit: ");
-        fahrenheit=validarEntradaFloat();
+        printf("\nIngrese la temperatura en Fahrenheit: ");
+        fahrenheit = validarEntradaFloat();
         printf("La temperatura en Kelvin es: %.2f\n", fahrenheit_to_kelvin(fahrenheit));
+        pausa();
         break;
     case 6:
-        printf("Ingrese la temperatura en Kelvin: ");
-        kelvin=validarEntradaFloat();
+        printf("\nIngrese la temperatura en Kelvin: ");
+        kelvin = validarEntradaFloat();
         printf("La temperatura en Fahrenheit es: %.2f\n", kelvin_to_fahrenheit(kelvin));
+        pausa();
         break;
     default:
-        printf("Opcion incorrecta\n");
+        printf("\nOpcion incorrecta\n");
+        pausa();
         break;
     }
 }
-float validarEntradaFloat() {
+float validarEntradaFloat()
+{
     float numero;
     char entrada[100];
 
-    while (1) {
+    while (1)
+    {
         printf("Ingrese una temperatura: ");
         fgets(entrada, sizeof(entrada), stdin);
 
         // Verificar si la entrada es un número flotante válido
-        if (sscanf(entrada, "%f", &numero) == 1) {
+        if (sscanf(entrada, "%f", &numero) == 1)
+        {
             return numero;
-        } else {
-            printf("Error: La entrada no es un número flotante válido.\n");
+        }
+        else
+        {
+            printf("Error: La entrada no es un numero flotante valido.\n");
         }
     }
+}
+int validarEntradaInt()
+{
+    int numero;
+    char entrada[100];
+    while (1)
+    {
+        printf("Ingrese una opcion: ");
+        fgets(entrada, sizeof(entrada), stdin);
+        // Verificar si la entrada es un número entero válido
+        if (sscanf(entrada, "%d", &numero) == 1)
+        {
+            return numero;
+        }
+        else
+        {
+            printf("Error: La entrada no es un numero entero valido.\n");
+        }
+    }
+}
+void pausa()
+{
+    printf("Presiona una tecla para continuar...");
+    getchar();
 }
