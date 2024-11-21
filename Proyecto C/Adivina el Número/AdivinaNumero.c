fc7128c96274437a1aa664fce99188d9021ef510
StalkerCode <demyu369@gmail.com>
2024-11-21 16:05:01 -0500

    C Adivina el Número, inicio y rand

---
+++ Proyecto C/Adivina el Número/AdivinaNumero.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+void validarEntero(){
+    
+}
+int main()
+{
+    srand(time(NULL));
+    int numero = (rand() % 100) + 1;
+    
+    return 0;
+}
