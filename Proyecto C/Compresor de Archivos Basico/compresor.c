33f620774ab59e4cfa4169af818e2c30182ff29d
ShadowCodeStalker <demyu369@gmail.com>
2025-03-12 13:07:33 -0500

    C Compresor de Archivos, Fin

--- Proyecto C/Compresor de Archivos Basico/compresor.c
+++ Proyecto C/Compresor de Archivos Basico/compresor.c
@@ -2,15 +2,71 @@
 #include <string.h>
 #include <stdlib.h>
 
+/* capasidad maxima para la lectura*/
 const int MAX_BUFFER = 100;
 const int MAX_BUFFER_AUX = 100;
 const int MAX_ANALISIS = 3;
 
+char *crearCadena();
+char *compresorCadena(char *);
+void comprecion(char *);
+
+int main()
+{
+    char *nombreArchivo = NULL;
+    nombreArchivo = crearCadena();
+    comprecion(nombreArchivo);
+    free(nombreArchivo);
+    return 0;
+}
 /*
-el compresor de cadena tiene que funcionar con memoria dinamica
 el bufer de un maximo de 100 caracteres
 */
 
+char *crearCadena()
+{
+    char *Cadena = NULL;
+    const int tm = 50;
+    char buffer[tm];
+
+    while (1)
+    {
+        printf("ingrese el nombre del archivo( max 50 caracteres) que:");
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+        {
+            fprintf(stderr, "Error al ingresar caracteres\n");
+            continue;
+        }
+        int len = 0;
+        len = strlen(buffer);
+
+        if (len > 0 && buffer[len - 1] == '\n')
+            buffer[--len] = '\0';
+
+        if (len == 0)
+        {
+            fprintf(stderr, "No se ingresaron caracteres\n");
+            continue;
+        }
+
+        if (buffer[0] == ' ')
+        {
+            fprintf(stderr, "La cadena no debe comenzar con un espacio\n");
+            continue;
+        }
+
+        Cadena = (char *)malloc((len + 1) * sizeof(char));
+        if (Cadena == NULL)
+        {
+            perror("Error al asignar memoria");
+            exit(EXIT_FAILURE); // Terminar el programa si no hay memoria
+        }
+        strcpy(Cadena, buffer);
+        break;
+    }
+
+    return Cadena;
+}
 char *compresorCadena(char *cadena)
 {
     int contador = 1;
@@ -41,7 +97,6 @@ char *compresorCadena(char *cadena)
         }
         i++;
     }
-    printf("\nCadena buffer: %s\n", buffer);
     int longitudTotal = strlen(buffer) + 1;
     comprimida = (char *)malloc(longitudTotal * sizeof(char));
     if (comprimida == NULL)
@@ -54,9 +109,24 @@ char *compresorCadena(char *cadena)
     return comprimida;
 }
 
-void leerArchivo(char *nombreArchivo)
+void comprecion(char *nombreArchivo)
 {
     FILE *archivo = fopen(nombreArchivo, "r");
+    char *nombreComprimido = NULL;
+    if (archivo == NULL)
+    {
+        printf("Error al abrir el archivo\n");
+        return;
+    }
+    printf("Ingrese el nombre del archivo comprimido\n");
+    printf("Ejemplo: archivo.txt\n");
+    nombreComprimido = crearCadena();
+    if (nombreComprimido == NULL)
+    {
+        printf("Error al crear el nombre del archivo comprimido\n");
+        return;
+    }
+    FILE *archivo2 = fopen(nombreComprimido, "w");
     if (archivo == NULL)
     {
         printf("Error al abrir el archivo\n");
@@ -66,15 +136,10 @@ void leerArchivo(char *nombreArchivo)
     while (fgets(linea, sizeof(linea), archivo))
     {
         char *comprimida = compresorCadena(linea);
-        printf("Cadena comprimida: %s\n", comprimida);
+        fputs(comprimida, archivo2);
         free(comprimida);
     }
+    free(nombreComprimido);
     fclose(archivo);
-}
-
-int main()
-{
-    leerArchivo("sapo.txt");
-
-    return 0;
+    fclose(archivo2);
 }
