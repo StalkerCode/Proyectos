13564637c00fd6989ec73b812ed5a26f012d2c52
ShadowCodeStalker <demyu369@gmail.com>
2025-03-17 14:44:19 -0500

    C Administrador de Tareas, funciones primarias

---
+++ Proyecto C/Administrador de Tareas/test.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Tarea
+{
+    int id;
+    char descripcion[100];
+    int completada; // 0 = No, 1 = Sí
+} Tarea;
+
+typedef struct ListaTareas
+{
+    Tarea tarea;
+    struct listaTareas *siguiente;
+} ListaTareas;
+
+void limpiarBuffer()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+    // Limpia el buffer de entrada
+}
+
+void crearDescripcion(char *descripcion)
+{
+    char buffer[100];
+    while (1)
+    {
+        printf(":");
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+        {
+            fprintf(stderr, "Error al ingresar caracteres\n");
+            continue;
+        }
+        int len = 0;
+        len = strlen(buffer);
+        // limpia el buffer si hay mas de 49 caarcteres
+        if (len >= 100 - 1 && buffer[len - 1] != '\n')
+            limpiarBuffer();
+
+        // Eliminar salto de línea si existe
+        if (len > 0 && buffer[len - 1] == '\n')
+            buffer[--len] = '\0';
+
+        // Verificar condiciones de validez
+        if (len == 0)
+        {
+            fprintf(stderr, "No se ingresaron caracteres\n");
+            continue;
+        }
+
+        if (buffer[0] == ' ')
+        {
+            fprintf(stderr, "La cadena no debe comenzar con un espacio\n");
+            continue;
+        }
+
+        strcpy(descripcion, buffer);
+        break;
+    }
+}
+
+int crearEntero()
+{
+
+    int entero;
+    // Verifica si la entrada es un número entero válido
+    while (1) {
+        if (scanf("%d", &entero) != 1) {
+            fprintf(stderr, "Error: Ingrese un número entero válido\n");
+            limpiarBuffer();
+            printf(":");
+            continue;
+        }
+        break;
+    }
+    return entero;
+}
+ 
+
+Tarea creraTarea()
+{
+    Tarea t1;
+    printf("Ingrese el Id de la tarea: ");
+    t1.id = crearEntero();
+    limpiarBuffer();
+    printf("Ingrese la descripcion de la tarea");
+    crearDescripcion(t1.descripcion);
+    t1.completada = 0;
+    return t1;
+}
+
+
+
+int main()
+{
+    Tarea t1;
+    t1 = creraTarea();
+    printf("Id: %d\n", t1.id);
+    printf("Descripcion: %s\n", t1.descripcion);
+    printf("Completada: %d\n", t1.completada);
+    return 0;
+}
