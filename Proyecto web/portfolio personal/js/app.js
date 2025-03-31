6997f8b9b38378d2708a3977994554384aa2b375
ShadowCodeStalker <demyu369@gmail.com>
2025-03-31 10:54:49 -0500

    Web Portfolio Personal, Fin

--- Proyecto web/portfolio personal/js/app.js
+++ Proyecto web/portfolio personal/js/app.js
@@ -1,19 +1,24 @@
-let principal=()=>{
-    caveza();
-    menu();
-    
-}
-
-let caveza=()=>{
-    let cabesero=document.getElementById("cabesero"); 
-    cabesero.innerHTML="<h1>Portafolio Personal</h1>";
+const principal = () => {
+    cabecera();
+    crearMenu();
+    pieDePagina();
 };
 
-let menu=()=>{
-    let menu=document.getElementById("menu");
-    menu.innerHTML=`
-                <li class="boton"><a href="/indes.html">inicio</a></li>
-                <li class="boton"><a href="/pages/about_me.html">sobre mi</a></li>
-                <li class="boton"><a href="/pages/projects.html">proyectos</a></li>
-            `;
+const cabecera = () => {
+    const cabeceraElement = document.getElementById("cabesero");
+    cabeceraElement.innerHTML = "<h1>Portafolio Personal</h1>";
+};
+
+const crearMenu = () => {
+    const menuElement = document.getElementById("menu");
+    menuElement.innerHTML = `
+        <li class="boton"><a href="/index.html">Inicio</a></li>
+        <li class="boton"><a href="/pages/about_me.html">Sobre mí</a></li>
+        <li class="boton"><a href="/pages/projects.html">Proyectos</a></li>
+    `;
+};
+
+const pieDePagina = () => {
+    const pieElement = document.getElementById("pie");
+    pieElement.textContent = "© 2025 ShadowCodeStalker. Todos los derechos reservados.";
 };
