e365ce51c0d947f8861119c2d97e5538221ab1f8
ShadowCodeStalker <demyu369@gmail.com>
2025-04-02 17:17:49 -0500

    Julia Calculadora Cientifica, Menus basico y avanzado

--- Proyecto julia/CalculadoraCientifica/src/main.jl
+++ Proyecto julia/CalculadoraCientifica/src/main.jl
@@ -15,7 +15,7 @@ function menuPrincipal()
     println("3. Operaciones Trigonometrica")
     println("4. Operaciones Utiles")
     println("5. Salir")
-    print("Seleccione una opción: ")
+    print("Seleccione una opción")
 end
 
 function menuBasico()
@@ -26,7 +26,7 @@ function menuBasico()
     println("4. División")
     println("5. Modulo")
     println("6. Volver")
-    print("Seleccione una opción: ")
+    print("Seleccione una opción")
 end
 
 function menuAvanzado()
@@ -38,7 +38,7 @@ function menuAvanzado()
     println("5. Logaritmo Base N")
     println("6. Elurer a la N")
     println("7. Volver")
-    print("Seleccione una opción: ")
+    print("Seleccione una opción")
 end
 
 function menuUtiles()
@@ -47,7 +47,7 @@ function menuUtiles()
     println("2. Maximo Comun Divisor")
     println("3. Minimo Comun Multiplo")
     println("4. Volver")
-    print("Seleccione una opción: ")
+    print("Seleccione una opción")
 end
 
 function menuTrigonometrico()
@@ -61,7 +61,117 @@ function menuTrigonometrico()
     println("6. Radianes a Grados")
     println("7. Grados a Radianes")
     println("8. Volver")
-    print("Seleccione una opción: ")
+    print("Seleccione una opción")
 end
 
-println(exp_n(1))  # Output: 2.718281828459045
+function ingresarEntero()
+    while true
+        print(": ")
+        try
+            return parse(Int, readline())
+        catch
+            println("ingrese un entero valido")
+        end
+    end
+end
+
+function ingresarNumero()
+    while true
+        print(": ")
+        try
+            return parse(Float32, readline())
+        catch
+            println("ingrese un entero valido")
+        end
+    end
+end
+function proseso(operacion)
+    println("Ingrese el primer numero")
+    num1 = ingresarNumero()
+    println("Ingrese el segundo numero")
+    num2 = ingresarNumero()
+    return operacion(num1, num2)
+end
+
+function basico()
+    while true
+        menuBasico()
+        op = ingresarEntero()
+        resultado = 0
+        if op == 1
+            resultado = proseso(add)
+        elseif op == 2
+            resultado = proseso(subtract)
+        elseif op == 3
+            resultado = proseso(multiply)
+        elseif op == 4
+            resultado = proseso(divide)
+            resultado = trunc(resultado, digits=2)
+        elseif op == 5
+            resultado = proseso(modulus)
+            resultado = trunc(resultado, digits=2)
+        elseif op == 6
+            return
+        else
+            println("opcion equivocada")
+            continue
+        end
+        println("El resultado es: ", resultado)
+    end
+end
+
+function avanzadas()
+    while true
+        menuAvanzado()
+        op = ingresarEntero()
+        resultado = 0
+        num1, num2 = (0, 0)
+        if op == 1
+            println("Ingrese la base")
+            num1 = ingresarNumero()
+            println("Ingrese el exponente")
+            num2 = ingresarNumero()
+            resultado = power(num1, num2)
+            resultado = trunc(resultado, digits=2)
+        elseif op == 2
+            println("Ingrese numero")
+            num1 = ingresarNumero()
+            resultado = squate_root(num1)
+            resultado = trunc(resultado, digits=2)
+        elseif op == 3
+            println("Ingrese numero")
+            num1 = ingresarNumero()
+            println("Ingrese la raiz")
+            num2 = ingresarNumero()
+            resultado = root_n(num1, num2)
+            resultado = trunc(resultado, digits=2)
+        elseif op == 4
+            println("Ingrese el numero")
+            num1 = ingresarNumero()
+            resultado = log_nepher(num1)
+            resultado = trunc(resultado, digits=2)
+        elseif op == 5
+            println("Ingrese el numero")
+            num1 = ingresarNumero()
+            println("Ingrese el log")
+            num2 = ingresarNumero()
+            resultado = log_base(num1, num2)
+            resultado = trunc(resultado, digits=2)
+        elseif op == 6
+            println("Ingrese el exponente")
+            num1 = ingresarNumero()
+            resultado = exp_n(num1)
+            resultado = trunc(resultado, digits=2)
+        elseif op == 7
+            return
+        else
+            println("opcion equivocada")
+            continue
+        end
+
+        println("El resultado es: ", resultado)
+    end
+end
+
+basico()
+avanzadas()
