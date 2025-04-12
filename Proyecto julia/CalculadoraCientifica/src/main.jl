3f9ff76640ca4ae8b2a62f60851def364a0d4ef8
ShadowCodeStalker <demyu369@gmail.com>
2025-04-12 11:55:11 -0500

    Julia Calculadora Cientifica, Fin

--- Proyecto julia/CalculadoraCientifica/src/main.jl
+++ Proyecto julia/CalculadoraCientifica/src/main.jl
@@ -58,9 +58,9 @@ function menuTrigonometrico()
     println("4. Arco Seno")
     println("5. Arco Coseno")
     println("6. Arco Tangente")
-    println("6. Radianes a Grados")
-    println("7. Grados a Radianes")
-    println("8. Volver")
+    println("7. Radianes a Grados")
+    println("8. Grados a Radianes")
+    println("9. Volver")
     print("Seleccione una opción")
 end
 
@@ -86,9 +86,9 @@ function ingresarNumero()
     end
 end
 function proseso(operacion)
-    println("Ingrese el primer numero")
+    print("Ingrese el primer numero")
     num1 = ingresarNumero()
-    println("Ingrese el segundo numero")
+    print("Ingrese el segundo numero")
     num2 = ingresarNumero()
     return operacion(num1, num2)
 end
@@ -127,38 +127,38 @@ function avanzadas()
         resultado = 0
         num1, num2 = (0, 0)
         if op == 1
-            println("Ingrese la base")
+            print("Ingrese la base")
             num1 = ingresarNumero()
-            println("Ingrese el exponente")
+            print("Ingrese el exponente")
             num2 = ingresarNumero()
             resultado = power(num1, num2)
             resultado = trunc(resultado, digits=2)
         elseif op == 2
-            println("Ingrese numero")
+            print("Ingrese numero")
             num1 = ingresarNumero()
             resultado = squate_root(num1)
             resultado = trunc(resultado, digits=2)
         elseif op == 3
-            println("Ingrese numero")
+            print("Ingrese numero")
             num1 = ingresarNumero()
-            println("Ingrese la raiz")
+            print("Ingrese la raiz")
             num2 = ingresarNumero()
             resultado = root_n(num1, num2)
             resultado = trunc(resultado, digits=2)
         elseif op == 4
-            println("Ingrese el numero")
+            print("Ingrese el numero")
             num1 = ingresarNumero()
             resultado = log_nepher(num1)
             resultado = trunc(resultado, digits=2)
         elseif op == 5
-            println("Ingrese el numero")
+            print("Ingrese el numero")
             num1 = ingresarNumero()
-            println("Ingrese el log")
+            print("Ingrese el log")
             num2 = ingresarNumero()
             resultado = log_base(num1, num2)
             resultado = trunc(resultado, digits=2)
         elseif op == 6
-            println("Ingrese el exponente")
+            print("Ingrese el exponente")
             num1 = ingresarNumero()
             resultado = exp_n(num1)
             resultado = trunc(resultado, digits=2)
@@ -173,5 +173,107 @@ function avanzadas()
     end
 end
 
-basico()
-avanzadas()
+function trigonometrica()
+    while true
+        menuTrigonometrico()
+        op = ingresarEntero()
+        resultado = 0
+        num1 = 0.0
+        if op == 1
+            print("Ingrese el angulo(Radianes)")
+            num1 = ingresarNumero()
+            resultado = r_sin(num1)
+        elseif op == 2
+            print("Ingrese el angulo(Radianes)")
+            num1 = ingresarNumero()
+            resultado = r_cos(num1)
+        elseif op == 3
+            print("Ingrese el angulo(Radianes)")
+            num1 = ingresarNumero()
+            resultado = r_tan(num1)
+        elseif op == 4
+            print("Ingrese el numero")
+            num1 = ingresarNumero()
+            resultado = r_asin(num1)
+        elseif op == 5
+            print("Ingrese el numero")
+            num1 = ingresarNumero()
+            resultado = r_acos(num1)
+        elseif op == 6
+            print("Ingrese el numero")
+            num1 = ingresarNumero()
+            resultado = r_atan(num1)
+        elseif op == 7
+            print("Ingrese los radianes")
+            num1 = ingresarNumero()
+            resultado = rad_to_deg(num1)
+        elseif op == 8
+            print("Ingrese los angulos")
+            num1 = ingresarNumero()
+            resultado = deg_to_rad(num1)
+        elseif op == 9
+            return
+        else
+            println("opcion equivocada")
+            continue
+        end
+        println("El resultado es: ", resultado)
+    end
+end
+
+function utiles()
+    while true
+        menuUtiles()
+        op = ingresarEntero()
+        resultado = 0
+        num1, num2 = (0, 0)
+        if op == 1
+            print("Ingrese el numero para el factorial")
+            num1 = ingresarNumero()
+            resultado = factorial_n(num1)
+        elseif op == 2
+            print("Ingrese el primer numero")
+            num1 = ingresarNumero()
+            print("Ingrese el segundo numero")
+            num2 = ingresarNumero()
+            resultado = mcd(num1, num2)
+        elseif op == 3
+            print("Ingrese el primer numero")
+            num1 = ingresarNumero()
+            print("Ingrese el segundo numero")
+            num2 = ingresarNumero()
+            resultado = mcm(num1, num2)
+        elseif op == 4
+            return
+        else
+            println("opcion equivocada")
+            continue
+        end
+        println("El resultado es: ", resultado)
+    end
+end
+
+function Principal()
+    while true
+        menuPrincipal()
+        op = ingresarEntero()
+        if op == 1
+            basico()
+        elseif op == 2
+            avanzadas()
+        elseif op == 3
+            trigonometrica()
+        elseif op == 4
+            utiles()
+        elseif op == 5
+            println("Hasta luego")
+            return
+        else
+            println("opcion equivocada")
+            continue
+        end
+    end
+end
+
+Principal()
+println("\n\nfin del programa")
