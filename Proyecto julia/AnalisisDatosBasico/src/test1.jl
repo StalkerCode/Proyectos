b948fd7f4f6b791a781d9c7ec31ea6c9757e334a
ShadowCodeStalker <demyu369@gmail.com>
2025-04-30 15:00:18 -0500

    Julia Analisis Basico, cargar archivo 2.1

--- Proyecto julia/AnalisisDatosBasico/src/test1.jl
+++ Proyecto julia/AnalisisDatosBasico/src/test1.jl
@@ -4,20 +4,15 @@ using CSV
 # Cargar los datos de las flores desde el archivo CSV
 function cargar_hoja()
     path = "src/data/iris_data.csv"
-    contenido = ""
     datos = []
-    # Abre el archivo
     open(path, "r") do archivo
-        # Lee el archivo
-        contenido = read(archivo, String)
-        # crea una vertor de cada linea del archivo
-        lista = split(contenido, "\r\n")
-        for fila in lista
-            # convierte la vila en un vecto 
-            subfila=split(fila,",")
-            # convierte los datos en float
+        for linea in eachline(archivo)
+            # Omitir líneas vacías o que solo tienen espacios
+            if isempty(strip(linea))
+                continue
+            end
+            subfila = split(linea, ",")
             nueva_fila = Any[parse(Float64, dato) for dato in subfila[1:end-1]]
-            # agrega el tipo de flor al final de la fila
             push!(nueva_fila, subfila[end])
             push!(datos, nueva_fila)
         end
